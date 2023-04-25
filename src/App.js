89c66d5bf1bc73ac75c88767ff058b7a87481a51
DIGIPlus <admin@digiplus.cf>
2023-04-25 20:16:05 +0400

    Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React from "react"
 
-export default function App() {
+export default function Form() {
+    const [firstName, setFirstName] = React.useState("")
+    const [lastName, setLastName] = React.useState("")
+    /**
+     * Challenge: Track the applicant's last name as well
+     */
+    
+    console.log(firstName, lastName)
+    
+    function handleFirstNameChange(event) {
+        setFirstName(event.target.value)
+    }
+    
+    function handleLastNameChange(event) {
+        setLastName(event.target.value)
+    }
+    
     return (
         <form>
             <input
                 type="text"
                 placeholder="First Name"
+                onChange={handleFirstNameChange}
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleLastNameChange}
             />
         </form>
     )
