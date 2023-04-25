67883b880cafc797bc4cc214bc3ff91ae3f89580
DIGIPlus <admin@digiplus.cf>
2023-04-25 20:36:01 +0400

    1st and Lastname

--- src/App.js
+++ src/App.js
@@ -1,33 +1,73 @@
+// import React from "react"
+
+// export default function Form() {
+//     const [firstName, setFirstName] = React.useState("")
+//     const [lastName, setLastName] = React.useState("")
+//     /**
+//      * Challenge: Track the applicant's last name as well
+//      */
+    
+//     console.log(firstName, lastName)
+    
+//     function handleFirstNameChange(event) {
+//         setFirstName(event.target.value)
+//     }
+    
+//     function handleLastNameChange(event) {
+//         setLastName(event.target.value)
+//     }
+    
+//     return (
+//         <form>
+//             <input
+//                 type="text"
+//                 placeholder="First Name"
+//                 onChange={handleFirstNameChange}
+//             />
+//             <input
+//                 type="text"
+//                 placeholder="Last Name"
+//                 onChange={handleLastNameChange}
+//             />
+//         </form>
+//     )
+// }
+// replace with
+
 import React from "react"
 
 export default function Form() {
-    const [firstName, setFirstName] = React.useState("")
-    const [lastName, setLastName] = React.useState("")
+    const [fromData, setFormData] = React.useState(
+      {firstName: "", lastName: ""}
+      )
     /**
      * Challenge: Track the applicant's last name as well
      */
     
-    console.log(firstName, lastName)
-    
-    function handleFirstNameChange(event) {
-        setFirstName(event.target.value)
+    console.log(fromData)
+    function handledChange(event) {
+        //console.log(event.target.name)
+        setFormData(prevFormData => {
+          return {
+            ...prevFormData,
+            [event.target.name]: event.target.value
+          }
+        })
     }
-    
-    function handleLastNameChange(event) {
-        setLastName(event.target.value)
-    }
-    
+        
     return (
         <form>
             <input
                 type="text"
                 placeholder="First Name"
-                onChange={handleFirstNameChange}
+                onChange={handledChange}
+                name="firstName"
             />
             <input
                 type="text"
                 placeholder="Last Name"
-                onChange={handleLastNameChange}
+                onChange={handledChange}
+                name="lastName"
             />
         </form>
     )
